de26c9cfdde45a885ed04c5c1425b06e90d408dc
Caio Roberto Souza Santos <caiorss@ucl.br>
2021-07-02 17:13:23 -0300

    correcao botao na tela lista

--- script.js
+++ script.js
@@ -42,20 +42,20 @@ function carregaLista(idCampo) {
 function listar() {
     if (typeof(Storage) !== "undefined") {
         let produtos = localStorage.getItem("produtos");
-        document.write("<h1>Estoque:</h1>")
+        document.write("<h1>Lista:</h1>")
         if (produtos == null)
-            document.write("<h3>Ainda não há nenhum item no estoque</h3>");
+            document.write("<h3>Ainda não há nenhum item na Lista</h3>");
         else {
             produtos = JSON.parse(produtos);
             produtos.forEach(produto => {
                 document.write("<ul>");
                 document.write("<li>Nome do produto: "+produto.nome+"</li>");
                 document.write("<li>Código do produto: "+produto.codigo+"</li>");
-                document.write("<li>Quantidade no estoque: "+produto.quantidade+"</li>");
+                document.write("<li>Quantidade na Lista: "+produto.quantidade+"</li>");
                 document.write("</ul>");
             });
             // Aqui tento criar um botão via script para retornar a tela, porem não deu certo
-           // document.createElement(<input type="button" value="Tela Cadastro" onclick="window.open('cadastro.html','_self')">)
+           //document.createElement(<input type="button" value="Tela Cadastro" onclick="window.open('cadastro.html','_self')">)
         }
     }   
 }
