4f304c2106049ea4bd7ee34831b6bbd254b0157f
00markin <mvmarcus@ucl.br>
2021-07-06 00:37:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -19,14 +19,15 @@ function cadastrarProduto(produto, codig, nome, un, qtd, qr, atv) {
         ativo: atv
     };
 
+
+
     if (typeof (Storage) !== "undefined") {
         let produtos = localStorage.getItem("produtos");
         if (produtos == null) produtos = []; // Nenhum produto ainda foi cadastrado
         else produtos = JSON.parse(produtos);
         produtos.push(novoProduto); // Adiciona um novo produto
-        localStorage.setItem("listaProdutos", JSON.stringify(produtos))
+        localStorage.setItem("listaProdutos", JSON.stringify(produtos));
         alert("Foram cadastradas com sucesso " + qtd + " unidades do produto " + produto + "!");
-
         atualizarLista("totalLista");
         location.reload();
     }
@@ -44,28 +45,48 @@ function carregaLista(idCampo) {
     }
 }
 
-function coletar(params) {
-    
+function coletar(aux) {
+    let produtos = localStorage.getItem("listaProdutos");
+    let aColetar = [(aux+1)];
+    var aux2 = 0;
+    produtos.forEach(produto => {
+        var qtdc = document.getElementById(("qtdC"+aux2)).value;
+        if (qtdc != 0) {
+            let carrola = {
+                cod: produto.codigo,
+                qtd: (produto.qtidadeProduto -= qtdc) 
+            };
+            aColetar.push(carrola);
+        }
+    });
+    localStorage.setItem("listaCompras", JSON.stringify(aColetar));
 }
 
 
 function listar() {
     if (typeof (Storage) !== "undefined") {
-        let produtos = localStorage.getItem("produtos");
+        let produtos = localStorage.getItem("listaProdutos");
         document.write("<h1>Lista:</h1>")
         if (produtos == null)
             document.write("<h3>Ainda não há nenhum item na Lista</h3>");
         else {
             produtos = JSON.parse(produtos);
+            var aux = 0;
+
             produtos.forEach(produto => {
-                document.write("<ul>");
-                document.write("<li>Nome do produto: " + produto.nome + "</li>");
-                document.write("<li>Código do produto: " + produto.codigo + "</li>");
-                document.write("<li>Quantidade na Lista: " + produto.unidade + "</li>");
-                document.write("<li>Nome do produto: " + produto.qtidadeProduto + "</li>");
-                document.write("<li>Código do produto: " + produto.txtCodBarras + "</li>");
-                document.write("<li>Quantidade na Lista: " + produto.ativo + "</li>");
-                document.write("</ul>");
+                if (produto.ativo == "on") {
+                    document.write("<div>");
+                    document.write("<ul>");
+                    document.write("<li>Código do produto: " + produto.codigo + "</li>");
+                    document.write("<li>Nome do produto: " + produto.nome + "</li>");
+                    document.write("<li>Unidade " + produto.unidade + "</li>");
+                    document.write("<li>Quantidade na lista: " + produto.qtidadeProduto + "</li>");
+                    document.write("<li>Quantidade Comprada:<input id='qtdC"+ aux + "' type='number'><li>");
+                    document.write("<li>Código do produto: " + produto.txtCodBarras + "</li>");
+                    document.write("</ul>");
+                    document.write("</div>");
+                    aux++;
+                }
             });
         }
     }
